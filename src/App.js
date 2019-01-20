02ec6042c4519d18343c375871395212ff3d8ef4
Ignacio  Fernández Alvarez <nachoveiga1014@gmail.com>
2019-01-20 13:47:24 +0100

    added apollo client for graphql calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
 import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learning React!
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="App-intro">
+          {this.props.data.loading === true
+            ? "Loading"
+            : this.props.data.allPosts.edges.map(data => (
+                <ul key={data.node.title}>
+                  <li>{data.node.title}</li>
+                </ul>
+              ))}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const repoQuery = gql`
+  {
+    allPosts {
+      edges {
+        node {
+          title
+          body
+          author {
+            username
+          }
+        }
+      }
+    }
+  }
+`;
+
+const AppWithData = graphql(repoQuery, {
+  options: {
+    variables: {
+      name: "medellinjs"
+    }
+  }
+})(App);
+
+export default AppWithData;
