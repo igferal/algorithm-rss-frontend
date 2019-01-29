eb3ed12ede3fb128eb9e44285b2a627769554adb
Ignacio  Fernández Alvarez <nachoveiga1014@gmail.com>
2019-01-29 20:38:33 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import LoginComponent from "./Components/login/LoginComponent";
-
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import SignUpComponent from "./Components/signup/SignUpComponent";
 
 class App extends Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        <LoginComponent/>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" component={LoginComponent} exact={true} />
+          <Route path="/signup" component={SignUpComponent} exact={true} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     );
   }
 }
-
-
-
 
 export default App;
--- src/Components/signup/SignUpComponent.js
+++ src/Components/signup/SignUpComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Input, Icon, Button } from "antd";
-import "./logincomponent.css";
+import "./signUp.css";
 import axios from "axios";
-import { withRouter } from 'react-router-dom'
 
-class LoginComponent extends Component {
+class SignUpComponent extends Component {
   state = {
     username: "",
+    email: "",
+    name: "",
+    surname: "",
     password: ""
   };
 
@@ -32,12 +34,12 @@ class LoginComponent extends Component {
         password: this.state.password
       })
       .then(response => {
-        this.props.history.push('/signUp')
+        console.log(response.data);
       });
   }
 
   render() {
-    
+    console.log(this.props);
     return (
       <section className="form-container">
         <form action="">
@@ -48,13 +50,33 @@ class LoginComponent extends Component {
             onChange={this.onChangeUserName}
             value={this.state.username}
           />
+          <Input
+            placeholder="Email"
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={e => this.setState({ email: e.target.value })}
+            value={this.state.email}
+          />
+
+          <Input
+            placeholder="Nombre"
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={e => this.setState({ name: e.target.value })}
+            value={this.state.name}
+          />
+
+          <Input
+            placeholder="Apellido"
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={e => this.setState({ surname: e.target.value })}
+            value={this.state.surname}
+          />
+
           <Input.Password
             prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
             placeholder="Algo que no sea 123..."
             onChange={this.onChangePassword}
             value={this.state.password}
           />
-          
           <Button type="primary" onClick={this.sendForm} block>
             Login
           </Button>
@@ -64,4 +86,4 @@ class LoginComponent extends Component {
   }
 }
 
-export default LoginComponent;
+export default SignUpComponent;
